5428671140b9489e58013274a9ee35b1a70d7d95
Ryan Smith <smithr38@wit.edu>
2024-12-11 17:37:04 -0500

    add tab navigation

--- src/routes/app/app.jsx
+++ src/routes/app/app.jsx
@@ -1,14 +1,29 @@
-import { Link, Outlet } from "react-router-dom";
+import { Link, Outlet, useLocation } from "react-router-dom";
 import "./app.css";
 
 function App() {
+  const location = useLocation();
+
+  const isActive = (path) => {
+    return location.pathname === path ? "selected" : "";
+  };
+
   return (
     <>
       <nav className="top-nav">
-        <Link to="/">Home</Link>
-        <Link to="projects">Projects</Link>
-        <Link to="experience">Experience</Link>
-        <Link to="about">About Me</Link>
+        <Link to="/" className={isActive("/")}>
+          Home
+        </Link>
+        <Link to="projects" className={isActive("/projects")}>
+          Projects
+        </Link>
+        <Link to="experience" className={isActive("/experience")}>
+          Experience
+        </Link>
+        <Link to="about" className={isActive("/about")}>
+          About Me
+        </Link>
+        {/* TODO: color theme switcher */}
       </nav>
 
       <Outlet />
