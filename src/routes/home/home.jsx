7002477d0f194b4f4d426737c3ffd051c5aec0d2
Ryan Smith <smithr3873@gmail.com>
2025-04-15 20:59:41 -0400

    add skills to home page

--- src/routes/home/home.jsx
+++ src/routes/home/home.jsx
@@ -12,17 +12,33 @@ function Home() {
             <p>{INTRO.p1}</p>
             <p>{INTRO.p2}</p>
             </div>
+					<div>
+						<p style={{ marginBottom: 8 }}>Skills:</p>
+						<div className="skills">
+							{/* Python, JS, HTML, CSS, C#, SQL, React */}
+							<img src="/skill_icons/python.svg" alt="Python" title="Python" />
+							<img src="/skill_icons/javascript.svg" alt="JavaScript" title="JavaScript" />
+							<img src="/skill_icons/html5.svg" alt="HTML" title="HTML" />
+							<img src="/skill_icons/css.svg" alt="CSS" title="CSS" />
+							<img src="/skill_icons/csharp.svg" alt="C#" title="C#" />
+							<img src="/skill_icons/mysql.svg" alt="SQL" title="SQL" />
+							<img src="/skill_icons/react.svg" alt="React" title="React" />
+						</div>
+					</div>
+					<div>
+						<p style={{ marginBottom: 8 }}>Links:</p>
           <div className="contact-links">
             <a href="https://github.com/smithr38atwit" target="_blank">
-              <img height={26} src="/contact_icons/github.svg" alt="github icon" />
+								<img height={26} src="/contact_icons/github.svg" alt="github" title="GitHub" />
             </a>
             <a href="https://www.linkedin.com/in/smithr38/" target="_blank">
-              <img width={26} src="/contact_icons/linkedin.svg" alt="linkedin icon" />
+								<img width={26} src="/contact_icons/linkedin.svg" alt="linkedin" title="LinkedIn" />
             </a>
             {/* TODO: add copy to clipboard option? */}
             <a href="mailto:smithr3873@gmail.com" target="_blank">
-              <img width={18} src="/contact_icons/gmail.svg" alt="gmail icon" />
+								<img width={18} src="/contact_icons/gmail.svg" alt="gmail" title="GMail" />
             </a>
+						</div>
           </div>
         </div>
         {/* TODO: add projects and experience highlights */}
